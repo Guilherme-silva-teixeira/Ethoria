2a0b1f3bed1c62aa698f47f1a70caf0555c2dc19
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-09-07 18:14:58 -0300

    Update intro.js

--- intro/intro.js
+++ intro/intro.js
@@ -1,29 +1,3 @@
-let leftButton = document.getElementById("leftButton").addEventListener("mousedown",
-    function()
-    {
-        this.style.transform = "translateY(3px)";
-        let leftButtonUp = document.getElementById("leftButton").addEventListener("mouseup",
-            function()
-            {
-                this.style.transform = "translateY(0px)";
-            }
-        );
-    }
-);
-
-let rigthButton = document.getElementById("rigthButton").addEventListener("mousedown",
-    function()
-    {
-        this.style.transform = "translateY(3px)";
-        let rigthButtonUp = document.getElementById("rigthButton").addEventListener("mouseup",
-            function()
-            {
-                this.style.transform = "translateY(0px)";
-            }
-        );
-    }
-);
-
 function gameStarts()
 {
     let outGameDiv = document.getElementById("main-page");
@@ -38,6 +12,7 @@ function gameStarts()
         </div>
     `;
     let EthoriaInitialPage = document.getElementById("EthoriaInitialPage");
+    EthoriaInitialPage.style.transition = "0.3s ease";
     EthoriaInitialPage.style.height = "105%";
     EthoriaInitialPage.style.width = "100%";
     EthoriaInitialPage.style.position = "absolute";
@@ -55,12 +30,18 @@ function gameStarts()
     let buttons = document.getElementById("buttons");
     buttons.style.display = "flex";
     buttons.style.flexDirection = "column";
+    window.style.transition = "0.3s ease";//Era para ter adicionado essa linha lá no css
+    buttons.style.transition = "0.3s ease";//acho que vai rodar
 
     let firstB = document.getElementById("startGame").addEventListener("mousedown",
         function()
         {
-            ifGameButtonStartClicked();
+            ifGameButtonStartClicked();//função para rodar o game
             //executar função de inicio dos slides do jogo
+            if(ifGameButtonStartClicked() == true)
+            {
+                textBox();
+            }
             this.style.transform = "translateY(3px)";
             this.addEventListener("mouseup",
                 function()
@@ -89,8 +70,11 @@ function gameStarts()
 function ifGameButtonStartClicked()
 {
     let ethoriaPage = document.getElementById("EthoriaInitialPage");
-    let wwindow = document.getElementById("window");
+    let window = document.getElementById("window");
     let buttons = document.getElementById("buttons");
+    ethoriaPage.style.transition = "0.3s ease";
+    window.style.transition = "0.3s ease";
+    buttons.style.transition = "0.3s ease";
     ethoriaPage.style.height = "0px";
     ethoriaPage.style.width = "0px";
     ethoriaPage.style.position = "absolute";
@@ -103,9 +87,55 @@ function ifGameButtonStartClicked()
     window.style.justifyContent = "none";
     window.style.alignItems = "none";
     window.style.flexDirection = "none";
+    window.style.zIndex = -3;
+    ethoriaPage.style.background = "#0000";
+    window.style.background = "#0000";
+    buttons.style.background = "#0000";
+    buttons.style.height = "0px";
+    buttons.style.width = "0px";
+    buttons.style.border = "0";
+    return true;
 }
 
 function textBox()
 {
+    let title = "teste";
+    let description = "descrição";
 
+    let windowMessage = document.getElementById("top");
+    windowMessage.innerHTML = `
+        <div id="message">
+            <div id="title">
+                <h1>${title}</h1>
+                </div>
+            <div id="description">
+                <p>${description}</h1>
+                </div>
+    `;
+
+    let leftButton = document.getElementById("leftButton").addEventListener("mousedown",
+        function()
+        {
+            this.style.transform = "translateY(3px)";
+            let leftButtonUp = document.getElementById("leftButton").addEventListener("mouseup",
+                function()
+                {
+                    this.style.transform = "translateY(0px)";
+                }
+            );
+        }
+    );
+    
+    let rigthButton = document.getElementById("rigthButton").addEventListener("mousedown",
+        function()
+        {
+            this.style.transform = "translateY(3px)";
+            let rigthButtonUp = document.getElementById("rigthButton").addEventListener("mouseup",
+                function()
+                {
+                    this.style.transform = "translateY(0px)";
+                }
+            );
+        }
+    );
 }
