f99e1627ce4c377fc35f99386eb6f5a6c6a9e743
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-09-08 17:26:25 -0300

    Update intro.js

--- intro/intro.js
+++ intro/intro.js
@@ -99,8 +99,34 @@ function ifGameButtonStartClicked()
 
 function textBox()
 {
-    let title = "teste";
-    let description = "descrição";
+    let title = ["O inicio",""];
+    let description = 
+    [
+        `Há muito tempo, em um reino distante chamado Ethoria,
+         os sábios eram conhecidos por sua habilidade em conjurar
+         magia ancestral, uma arte que combinava conhecimento arcano 
+         com ética e moralidade. No entanto, um grande dilema
+         assolava os corações dos habitantes de Ethoria.`,
+         //segundo parágrafo
+         `A sociedade de Ethoria era dividida em duas facções
+          principais: os Puristas e os Progressistas. Os Puristas
+          acreditavam na estrita adesão às tradições éticas ancestrais, 
+          enquanto os Progressistas defendiam a evolução das
+          práticas éticas para se adaptarem aos tempos modernos.`,
+         //terceiro parágrafo
+         `A contenda entre essas facções tornou-se mais intensa 
+         quando uma crise ameaçou o reino. Uma força maligna, 
+         conhecida como a Sombra Eterna, começou a se espalhar, 
+         corrompendo a terra e ameaçando destruir tudo em seu 
+         caminho.`,
+         //quarto parágrafo
+         `Os Puristas argumentavam que a única maneira de derrotar 
+         a Sombra Eterna era invocar os rituais ancestrais, 
+         que exigiam sacrifícios severos e o uso de magia proibida 
+         há muito tempo. Eles acreditavam que, ao seguir estritamente 
+         os ensinamentos éticos do passado, poderiam purificar a terra 
+         e salvar o reino.`,
+    ];
 
     let windowMessage = document.getElementById("top");
     windowMessage.innerHTML = `
