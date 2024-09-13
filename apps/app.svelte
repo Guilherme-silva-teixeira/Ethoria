eb0b9caa8545ef42d00a40cb2562489566bee23c
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-09-13 17:01:00 -0300

    Update app.svelte

--- apps/app.svelte
+++ apps/app.svelte
@@ -1,1 +1,7 @@
-
+<!DOCTYPE html><html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ethoria</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Intro/intro.css">
